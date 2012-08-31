d9d42cb55dda3d5fa72157279c042d444c129865
David Gullmarsvik <awt2.2012@gmail.com>
2012-08-31 13:29:13 +0200

    Utgångspunkt för övningar

---
+++ Lektion6/Models/Entities/User.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lektion6.Models.Entities.Abstract;
+using System.Security.Cryptography;
+
+namespace Lektion6.Models.Entities
+{
+    public class User : IEntity
+    {
+        public User() { ID = Guid.NewGuid(); }
+        public User(string name, UserType type)
+        {
+            ID = Guid.NewGuid();
+            UserName = name;
+            Type = type;
+        }
+
+        public Guid ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { 
+            get 
+            {
+                var data = Encoding.Unicode.GetBytes(UserName); // Hashfunktionen behöver en Byte-array som input
+                var hashData = new SHA1Managed().ComputeHash(data); // Här skapar vi vår hash
+                return Convert.ToBase64String(hashData); // Vi vill returnera en sträng-representation av hashen
+            } 
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(FirstName) && string.IsNullOrEmpty(LastName))
+                    return "NoName";
+                else if (string.IsNullOrEmpty(FirstName))
+                    return LastName;
+                /*
+                else if (string.IsNullOrEmpty(LastName))
+                    return FirstName;
+                */
+                else
+                    return string.Format("{0} {1}", FirstName, LastName);
+            }
+        }
+
+        public UserType Type { get; set; }
+
+        public string ToString(bool ShortFormat = true)
+        {
+            string userString = string.Format("\tFullName: '{0}' - UserID: '{1}'", FullName, ID);
+            if (!ShortFormat)
+                userString += string.Format("\n\t\tUserName: '{0}' - UserType: '{1}'", UserName, Type);
+            return userString;
+        }
+
+        public enum UserType
+        {
+            User,
+            Admin,
+            SuperUser
+        }
+    }
+}
---
+++ Lektion6/Models/Repositories/Repository.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lektion6.Models.Repositories.Abstract;
+using Lektion6.Models.Entities;
+using Lektion6.Models.Entities.Abstract;
+using System.Collections;
+using Lektion6.Models.Repositories.Helpers;
+
+namespace Lektion6.Models.Repositories
+{
+    /// <summary>
+    /// Klass som hanterar data för applikationen
+    /// </summary>
+    public class Repository : IRepository
+    {
+        // privata listor med User- och Post-objekt
+        private Dictionary<string,IList> dataSource;
+
+        /// <summary>
+        /// Returns the instance of the repository (Makes sure the repository is only created once - Using the Singleton Design Pattern
+        /// </summary>
+        public static Repository Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot) // Thread-Safe Repository since we are in a multithread environment
+                    {
+                        if (instance == null)
+                            instance = new Repository();
+                    }
+                }
+
+                return instance;
+            }
+        }
+        private static volatile Repository instance;
+        public static object syncRoot = new Object();
+
+        /// <summary>
+        /// Privat Konstruktor som genererar 36 dummy Users och 100 dummy Posts
+        /// </summary>
+        private Repository()
+        {
+            // Här inne genererar vi dummy-data. För denna lab behöver du inte veta hur denna data genereras.
+            List<Guid> UserIDs;
+            List<User> users = DummyDataGenerators.GenerateUsers(out UserIDs); // För nyckelordet 'out' se kommentar innan definitionen av GenerateUsers
+            List<Post> posts = DummyDataGenerators.GeneratePosts(UserIDs);
+            List<ForumThread> forumThreads = new List<ForumThread>();
+            List<News> news = DummyDataGenerators.GenerateNews(UserIDs);
+
+            dataSource = new Dictionary<string, IList>();
+            dataSource.Add(users.GetType().GetGenericArguments()[0].FullName, users);
+            dataSource.Add(posts.GetType().GetGenericArguments()[0].FullName, posts);
+            dataSource.Add(forumThreads.GetType().GetGenericArguments()[0].FullName, forumThreads);
+            dataSource.Add(news.GetType().GetGenericArguments()[0].FullName, news);
+        }
+
+        /// <summary>
+        /// Returns a list of the specified Type
+        /// </summary>
+        /// <typeparam name="T">Valid types: User, Post, News or ForumThread</typeparam>
+        /// <returns></returns>
+        public List<T> All<T>() where T : IEntity
+        {
+            if (!dataSource.ContainsKey(typeof(T).FullName))
+                throw new Exception("Unsupported Type!");
+
+            return (List<T>)dataSource[typeof(T).FullName];
+        }
+
+        /// <summary>
+        /// Returns an entity from the datasource with the specified ID or null if the ID doesn't exist
+        /// 
+        /// Usage: User myUser = myRepository.Get<User>(id);
+        /// </summary>
+        /// <typeparam name="T">Valid types: User, Post, News or ForumThread</typeparam>
+        /// <param name="id">ID of the Entity</param>
+        /// <returns></returns>
+        public T Get<T>(Guid id) where T : IEntity
+        {
+            return All<T>().Where(t => t.ID == id).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Deletes the specified entity
+        /// </summary>
+        /// <typeparam name="T">Valid types: User, Post, News or ForumThread</typeparam>
+        /// <param name="entity">The entity to delete</param>
+        public void Delete<T>(T entity) where T : IEntity
+        {
+            if (!dataSource.ContainsKey(typeof(T).FullName))
+                throw new Exception("Unsupported Type!");
+
+            dataSource[typeof(T).FullName].Remove(entity);
+        }
+
+        /// <summary>
+        /// Saves an entity
+        /// </summary>
+        /// <typeparam name="T">Valid types: User, Post, News or ForumThread</typeparam>
+        /// <param name="entity">The entity to be saved</param>
+        public void Save<T>(T entity) where T : IEntity
+        {
+            List<T> list = All<T>();
+
+            if (list.Contains(entity))
+                list[list.IndexOf(entity)] = entity;
+            else
+                list.Add(entity);
+
+            dataSource[typeof(T).FullName] = list;
+        }
+    }
+}
