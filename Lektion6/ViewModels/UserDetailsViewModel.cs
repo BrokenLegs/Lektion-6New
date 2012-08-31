6d8291fbbc40fdca509f5a8cbf56564ddeea8660
David Gullmarsvik <awt2.2012@gmail.com>
2012-08-31 16:51:57 +0200

    Uppgift 29

--- Lektion6/ViewModels/UserDetailsViewModel.cs
+++ Lektion6/ViewModels/UserDetailsViewModel.cs
@@ -8,6 +8,7 @@ namespace Lektion6.ViewModels
 {
     public class UserDetailsViewModel
     {
-        
+        public User User { get; set; }
+        public List<Post> Posts { get; set; }
     }
 }
