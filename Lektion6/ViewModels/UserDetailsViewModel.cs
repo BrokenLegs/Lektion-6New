a0e9af4d0cf6c05447408973b07f2ffa0cc0881a
David Gullmarsvik <awt2.2012@gmail.com>
2012-08-31 16:51:43 +0200

    Uppgift 28

---
+++ Lektion6/ViewModels/UserDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lektion6.Models.Entities;
+
+namespace Lektion6.ViewModels
+{
+    public class UserDetailsViewModel
+    {
+        
+    }
+}
