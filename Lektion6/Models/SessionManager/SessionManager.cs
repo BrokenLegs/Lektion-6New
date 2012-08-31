3fefae7ab27aa12470fa67ade10b9f7fdd4d9cc2
David Gullmarsvik <awt2.2012@gmail.com>
2012-08-31 13:41:58 +0200

    Uppgift 2

---
+++ Lektion6/Models/SessionManager/SessionManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lektion6.Models.SessionManager
+{
+    public class SessionManager
+    {
+    }
+}
